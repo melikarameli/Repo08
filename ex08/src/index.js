ef092f15f611d60159889a8aa639d6c151ff25e0
melikarameli <melikahatunic2001@gmail.com>
2022-04-04 15:52:19 +0200

    Added ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+var total = 0;
+function funcWithArg(digit) {
+    return (digit + 9)/5;
+}
+var total = funcWithArg(6);
+funcWithArg();
+// Only change code above this line
+console.log(total);
+module.exports = funcWithArg;
