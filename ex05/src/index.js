aa9e7709a518cdc6926557e26ea03398fe54347b
melikarameli <melikahatunic2001@gmail.com>
2022-04-04 15:12:27 +0200

    Added ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+    console.log("sum from addThree: ", sum);
+}
+function addFive() {
+    sum = sum + 5;
+    console.log("sum from addFive: ", sum);
+}
+console.log(addThree());
+console.log(addFive());
+// Only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
